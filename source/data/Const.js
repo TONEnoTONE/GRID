b18413e962d4fbc3b8b84c35f0ba358c4eca80a0
Yotam Mann <yotammann@gmail.com>
2013-09-17 19:03:19 -0400

    rererefactored const

--- source/data/Const.js
+++ source/data/Const.js
@@ -16,43 +16,46 @@ goog.provide("data.Const");
 	the container of constants
 	@typedef {Object}
 */
-var CONST = {
-	/** @enum {string} */
-	DIRECTION : {
-		NORTH : 'n',
-		SOUTH : 's',
-		EAST : 'e',
-		WEST : 'w'
-	},
-	/** @enum {string} */
-	DIRECTION.OPPOSITE : {
-		NORTH : CONST.DIRECTION.SOUTH,
-		SOUTH : CONST.DIRECTION.NORTH,
-		EAST : CONST.DIRECTION.WEST,
-		WEST : CONST.DIRECTION.EAST
-	},
-	/** @enum {number}*/
-	TILE : {
-		INACTIVE : 0,
-		ACTIVE : 1
-	},
-	/** 
-		the size of the grid 
-		@const
-	*/
-	SIZE : {
-		WIDTH : 8,
-		HEIGHT : 8
-	},
+var CONST = {}
 
-	/** 
-		the app states
-		@const
-	*/
-	APPSTATES : {
-		SCREEN_LOADING : 'SCREEN_LOADING',
-		SCREEN_SONGS : 'SCREEN_SONGS',
-		SCREEN_PARTS : 'SCREEN_PARTS',
-		SCREEN_GAME : 'SCREEN_GAME'
-	}
-};
+/** @enum {string} */
+CONST.DIRECTION = {
+	NORTH : 'n',
+	SOUTH : 's',
+	EAST : 'e',
+	WEST : 'w'
+}
+
+/** @enum {string} */
+CONST.DIRECTION.OPPOSITE = {
+	NORTH : CONST.DIRECTION.SOUTH,
+	SOUTH : CONST.DIRECTION.NORTH,
+	EAST : CONST.DIRECTION.WEST,
+	WEST : CONST.DIRECTION.EAST
+}
+/** @enum {number} */
+CONST.TILE = {
+	INACTIVE : 0,
+	ACTIVE : 1,
+	FORWARDSLASH : 2, //a forward diagonal '/'
+	BACKSLASH : 3 //a back diagonal '\'
+}
+/** 
+	the size of the grid 
+	@const
+*/
+CONST.SIZE = {
+	WIDTH : 8,
+	HEIGHT : 8
+}
+
+/** 
+	the app states
+	@const
+*/
+CONST.APPSTATES = {
+	SCREEN_LOADING : 'SCREEN_LOADING',
+	SCREEN_SONGS : 'SCREEN_SONGS',
+	SCREEN_PARTS : 'SCREEN_PARTS',
+	SCREEN_GAME : 'SCREEN_GAME'
+}
