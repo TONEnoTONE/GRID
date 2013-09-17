4e6b0a47d0739466c80616cf8462bb7ce6167772
Yotam Mann <yotammann@gmail.com>
2013-09-17 15:04:38 -0400

    scoping for GameController

--- source/game/controllers/GameController.js
+++ source/game/controllers/GameController.js
@@ -14,28 +14,36 @@ goog.require("game.controllers.PieceController");
 goog.require("game.controllers.TileController");
 
 
-game.controllers.GameController = {
+goog.scope(function(){
+
+	//alias
+	var GameController = game.controllers.GameController;
+
 	/** initializer */
-	initialize : function(){
+	GameController.initialize = function(){
 		/** @this {game.controllers.GameController} */
-		game.controllers.GameController.setStage(0, 0);
-	},
+		GameController.setStage(0, 0);
+	}
+
 	/** 
 		goes to the next level
 	*/
-	nextLevel : function(){
+	GameController.nextLevel = function(){
 
-	},
+	}
+
 	/** 
 		@param {number} stage
 		@param {number=} level
 	*/
-	setStage : function(stage, level){
+	GameController.setStage = function(stage, level){
 		level = level||0;
 		//setup the map
 		game.controllers.TileController.setStage(stage, level);
 	}
-}
 
-//run initializer
-game.controllers.GameController.initialize();
+	//run initializer
+	GameController.initialize();
+
+});
+
