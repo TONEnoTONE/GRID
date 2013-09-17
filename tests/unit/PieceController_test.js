4901f1fe607903eea2c4de5f668e9a1a93454c4b
Yotam Mann <yotammann@gmail.com>
2013-09-17 17:46:31 -0400

    fixed tests

--- tests/unit/PieceController_test.js
+++ tests/unit/PieceController_test.js
@@ -6,7 +6,7 @@ goog.require('dependencies.chai');
 
 function testAddPiece(){
 	var position = {x : 3, y : 3};
-	game.controllers.PieceController.addPiece({x : 3, y : 3});
-	var p = game.controllers.PieceController.pieceAt({x : 3, y : 3});
+	PieceController.addPiece({x : 3, y : 3});
+	var p = PieceController.pieceAt({x : 3, y : 3});
 
 }
