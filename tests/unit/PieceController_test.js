26c89b7ae7d56cc9e933dfd300ce831ff8fdd571
Yotam Mann <yotammann@gmail.com>
2013-09-17 19:07:23 -0400

    extra verification in pieceAt test

--- tests/unit/PieceController_test.js
+++ tests/unit/PieceController_test.js
@@ -1,12 +1,17 @@
 goog.require('game.controllers.PieceController');
+goog.require('goog.math.Coordinate');
 goog.require('data.Const');
 goog.require('goog.testing.jsunit');
 goog.require('dependencies.chai');
 
 
-function testAddPiece(){
-	var position = {x : 3, y : 3};
-	PieceController.addPiece({x : 3, y : 3});
-	var p = PieceController.pieceAt({x : 3, y : 3});
-
+function testAddAndGetPiece(){
+	var position = new goog.math.Coordinate(3, 3);
+	PieceController.addPiece(position);
+	var p = PieceController.pieceAt(position);
+	chai.expect(p.position.x).to.equal(3);
+	chai.expect(p.position.y).to.be.a("number");
+	var position2 = new goog.math.Coordinate(4, 3);
+	var p2 = PieceController.pieceAt(position2);
+	chai.expect(p2).to.be.null;
 }
