02b40b611967966a451e73c69b7c520f5d6e3678
Chris Deaner <deaner@monsterbit.com>
2013-09-20 23:45:34 -0400

    first commit for ScreenControllerTest

---
+++ tests/unit/ScreenController_test.js
@@ -0,0 +1,22 @@
+goog.require('goog.testing.jsunit');
+goog.require('dependencies.chai');
+
+goog.require("data.Const");
+
+goog.require('screens.ScreenController'); 
+goog.require('screens.views.SplashScreen'); 
+goog.require('screens.views.SongsScreen'); 
+
+function testShowHideScreens(){
+	// SplashScreen
+	ScreenController.showScreen(CONST.APPSTATES.SCREEN_SPLASH);	// show
+	chai.expect(SplashScreen.div.style.display).to.be.equal('');
+	ScreenController.hideScreen(CONST.APPSTATES.SCREEN_SPLASH);	// hide
+	chai.expect(SplashScreen.div.style.display).to.be.equal('none');
+	
+	// SongsScreen
+	ScreenController.showScreen(CONST.APPSTATES.SCREEN_SONGS);	// show
+	chai.expect(SongsScreen.div.style.display).to.be.equal('');
+	ScreenController.hideScreen(CONST.APPSTATES.SCREEN_SONGS);	// hide
+	chai.expect(SongsScreen.div.style.display).to.be.equal('none');
+}
