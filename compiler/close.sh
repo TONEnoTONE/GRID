22a044bf095bc356ebc0adfe5a029caedf022986
Chris Deaner <deaner@monsterbit.com>
2013-10-02 17:59:33 -0400

    nade compuiked really go to the compiled file

--- compiler/close.sh
+++ compiler/close.sh
@@ -16,7 +16,7 @@ python ../source/dependencies/closure-library/closure/bin/build/closurebuilder.p
 --compiler_flags="--jscomp_error=const" \
 --compiler_flags="--jscomp_error=constantProperty" \
 --compiler_flags="--warning_level=VERBOSE" \
---compiler_flags="--define='CONFIG.PLATFORM_TEST=\"iphone\"'"
+--compiler_flags="--define='CONFIG.PLATFORM=\"iphone\"'"
 
 
 # echo "compiling grid.js"
