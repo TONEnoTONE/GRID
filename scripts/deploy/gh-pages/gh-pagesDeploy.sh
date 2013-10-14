3472302a58129124cadcee014ec45920426abc9f
Chris Deaner <deaner@monsterbit.com>
2013-10-14 17:40:52 -0400

    crazy git and travis shiv.....

--- scripts/deploy/gh-pages/gh-pagesDeploy.sh
+++ scripts/deploy/gh-pages/gh-pagesDeploy.sh
@@ -5,6 +5,8 @@ here=$(pwd)
 echo "\n#################################################################"
 echo "github stuff"
 echo "#################################################################"
-cp $root/compiled.html $root/index.html
 
-git branch
+git checkout gh-pages
+cp $root/compiled.html $root/index.html
+git commit -m "committing from travis!"
+git push origin gh-pages 
