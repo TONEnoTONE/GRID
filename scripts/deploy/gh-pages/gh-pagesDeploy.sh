8217788075d0c943fe73f540e2054653217db1e7
Chris Deaner <deaner@monsterbit.com>
2013-10-14 17:54:28 -0400

    Update gh-pagesDeploy.sh

--- scripts/deploy/gh-pages/gh-pagesDeploy.sh
+++ scripts/deploy/gh-pages/gh-pagesDeploy.sh
@@ -6,6 +6,9 @@ echo "\n#################################################################"
 echo "github stuff"
 echo "#################################################################"
 
+git config --global user.email "deaner@monsterbit.com"
+git config --global user.name "lilshtz"
+
 git checkout gh-pages
 cp $root/compiled.html $root/index.html
 git commit -m "committing from travis!"
