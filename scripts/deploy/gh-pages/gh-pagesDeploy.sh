0866ebf2d678a121a91f91cc3c1eb9e763757a24
Chris Deaner <deaner@monsterbit.com>
2013-10-14 21:19:00 -0400

    added a fetch before checking out the branch

--- scripts/deploy/gh-pages/gh-pagesDeploy.sh
+++ scripts/deploy/gh-pages/gh-pagesDeploy.sh
@@ -9,6 +9,7 @@ echo "#################################################################"
 git config user.email "deaner@monsterbit.com"
 git config user.name "lilshtz"
 
+git fetch
 git checkout gh-pages
 cp $root/compiled.html $root/index.html
 git commit -m "committing from travis!"
