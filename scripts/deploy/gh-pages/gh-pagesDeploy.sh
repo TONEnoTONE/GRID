cacf8253421dbc0120759238ae2dbc74276e7316
Chris Deaner <deaner@monsterbit.com>
2013-10-14 21:26:18 -0400

    removed 'root' var and added a lot of verbosityness

--- scripts/deploy/gh-pages/gh-pagesDeploy.sh
+++ scripts/deploy/gh-pages/gh-pagesDeploy.sh
@@ -9,8 +9,13 @@ echo "#################################################################"
 git config user.email "deaner@monsterbit.com"
 git config user.name "lilshtz"
 
+echo "git fetch"
 git fetch
+echo "git checkout gh-pages"
 git checkout gh-pages
-cp $root/compiled.html $root/index.html
+echo "cp compiled.html index.html"
+cp compiled.html index.html
+echo "git commit"
 git commit -m "committing from travis!"
+echo "git push origin gh-pages"
 git push origin gh-pages 
