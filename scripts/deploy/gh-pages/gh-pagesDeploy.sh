790d8d44a0fc15189c10c658b5a5903ce2117272
Chris Deaner <deaner@monsterbit.com>
2013-10-15 00:46:16 -0400

    added remote directives for giting-er-done

--- scripts/deploy/gh-pages/gh-pagesDeploy.sh
+++ scripts/deploy/gh-pages/gh-pagesDeploy.sh
@@ -9,13 +9,23 @@ echo "#################################################################"
 git config user.email "deaner@monsterbit.com"
 git config user.name "lilshtz"
 
-echo "git fetch"
-git fetch
+echo "git remote set-url --push origin https://github.com/TONEnoTONE/GRID.git"
+git remote set-url --push origin https://github.com/TONEnoTONE/GRID.git
+
+echo "git fetch -v"
+git fetch -v
+
+echo "git remote set-branches --add origin gh-pages"
+git remote set-branches --add origin gh-pages
+
 echo "git checkout gh-pages"
 git checkout gh-pages
+
 echo "cp compiled.html index.html"
 cp compiled.html index.html
+
 echo "git commit"
 git commit -m "committing from travis!"
+
 echo "git push origin gh-pages"
 git push origin gh-pages 
