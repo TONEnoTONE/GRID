c2b64e2db10cdc7eedea39ed0389303560dd53b0
Yotam Mann <yotammann@gmail.com>
2013-10-17 18:17:21 -0400

    versiónêd

--- source/grid.js
+++ source/grid.js
@@ -19,7 +19,7 @@ goog.require("managers.Debug");
 //the application singleton
 var GRID = {
 	/** @const */
-	version : "0.0.3",
+	version : "0.0.4",
 	/** */
 	initialize : function(){
 		console.log("GRID version "+GRID.version);
