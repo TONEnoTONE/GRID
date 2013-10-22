96ebb04af96ed363f4d9e9e4e823ec69e1a40f1e
Chris Deaner <deaner@monsterbit.com>
2013-10-21 23:35:11 -0400

    it hurts when I don't -P

--- scripts/deploy/s3/s3.sh
+++ scripts/deploy/s3/s3.sh
@@ -7,6 +7,6 @@ cp $s3ScriptDir.s3cfg.tmp $s3ScriptDir.s3cfg
 echo "access_key = ${AWS_S3_KEY}" >> $s3ScriptDir.s3cfg
 echo "secret_key = ${AWS_S3_SECRET}" >> $s3ScriptDir.s3cfg
 
-s3cmd sync --config=$s3ScriptDir.s3cfg tmp/www/ s3://tonenotone.com/GRID/
+s3cmd sync --config=$s3ScriptDir.s3cfg -P tmp/www/ s3://tonenotone.com/GRID/
 
 rm $s3ScriptDir.s3cfg
