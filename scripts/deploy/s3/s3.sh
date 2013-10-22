ebbc8951b0ff4e9e039b9cbf55967955cd4778d8
Chris Deaner <deaner@monsterbit.com>
2013-10-21 21:47:21 -0400

    adding testing the key situation on travis.

--- scripts/deploy/s3/s3.sh
+++ scripts/deploy/s3/s3.sh
@@ -6,6 +6,9 @@ cp $s3ScriptDir.s3cfg.tmp $s3ScriptDir.s3cfg
 echo "access_key = ${AWS_S3_KEY}" >> $s3ScriptDir.s3cfg
 echo "secret_key = ${AWS_S3_SECRET}" >> $s3ScriptDir.s3cfg
 
+cat $s3ScriptDir.s3cfg
+
 s3cmd --config=$s3ScriptDir.s3cfg ls
+
 
 rm $s3ScriptDir.s3cfg
