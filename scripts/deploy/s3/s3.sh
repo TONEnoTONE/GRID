281f78147d60c325df5e8b0f86b96e5ae69046a9
Chris Deaner <deaner@monsterbit.com>
2013-10-21 23:28:59 -0400

    first attempt at an actual sync with s3

--- scripts/deploy/s3/s3.sh
+++ scripts/deploy/s3/s3.sh
@@ -3,9 +3,10 @@ s3ScriptDir="./scripts/deploy/s3/"
 
 cp $s3ScriptDir.s3cfg.tmp $s3ScriptDir.s3cfg
 
+# add the keys to the bottom of the s3cfg file.
 echo "access_key = ${AWS_S3_KEY}" >> $s3ScriptDir.s3cfg
 echo "secret_key = ${AWS_S3_SECRET}" >> $s3ScriptDir.s3cfg
 
-s3cmd --config=$s3ScriptDir.s3cfg ls
+s3cmd sync --config=$s3ScriptDir.s3cfg tmp/www/ s3://tonenotone.com/GRID/
 
-cat $s3ScriptDir.s3cfg
+rm $s3ScriptDir.s3cfg
