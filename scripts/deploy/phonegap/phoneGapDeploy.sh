3b4a366b5d5ed2b20df91a8560945e91539e5757
Chris Deaner <deaner@monsterbit.com>
2014-01-03 12:16:43 -0500

    changed the phonegap build settings to build to the echo app. added some comments delineating which app is which app id wise

--- scripts/deploy/phonegap/phoneGapDeploy.sh
+++ scripts/deploy/phonegap/phoneGapDeploy.sh
@@ -12,11 +12,20 @@ zip -r GRIDunLOCK.zip www
 echo "\n########################################"
 echo "uploading zip to phonegap"
 echo "########################################"
+# phonegap appid 619681 is ECHO
+# phonegap appid 708557 is Reflects
 curl -X PUT -F file=@./GRIDunLOCK.zip https://build.phonegap.com/api/v1/apps/619681?auth_token=$phoneGapToken
+#curl -X PUT -F file=@./GRIDunLOCK.zip https://build.phonegap.com/api/v1/apps/708557?auth_token=$phoneGapToken
+
+
 
 echo "\n########################################"
 echo "unlock the signing key"
 echo "########################################"
-curl -X PUT -d 'data={"keys":{"ios":{"id":110067,"password":"grid"}}}' https://build.phonegap.com/api/v1/apps/619681?auth_token=$phoneGapToken
+# phonegap appid 619681 is ECHO
+# phonegap appid 708557 is Reflects
+
+#curl -X PUT -d 'data={"keys":{"ios":{"id":110067,"password":"grid"}}}' https://build.phonegap.com/api/v1/apps/619681?auth_token=$phoneGapToken
+#curl -X PUT -d 'data={"keys":{"ios":{"id":110067,"password":"grid"}}}' https://build.phonegap.com/api/v1/apps/708557?auth_token=$phoneGapToken
 
 cd ../
