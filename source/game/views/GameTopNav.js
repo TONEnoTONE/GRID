20eff75eb99ff60e9a257905a6c0a615f0b815bb
Yotam <minibill@Minis-Mac-mini-2.local>
2014-01-16 18:07:08 -0500

    game screen styling. working on parts screen

--- source/game/views/GameTopNav.js
+++ source/game/views/GameTopNav.js
@@ -9,6 +9,9 @@ goog.provide("GamescreenTopBar");
 goog.require("goog.Disposable");
 goog.require("goog.dom");
 goog.require("goog.style");
+goog.require("goog.string");
+goog.require("screens.views.GridDom");
+goog.require("game.controllers.StageController");
 
 /** 
 	@constructor
@@ -16,6 +19,12 @@ goog.require("goog.style");
 */
 var GamescreenTopBar = function(){
 	goog.base(this);
+	/** @type {Element} */
+	this.Element = goog.dom.createDom("div", {"id" : "GamescreenTopBar"});
+	goog.dom.appendChild(GridDom.GameScreen, this.Element);
+	/** @type {Element} */
+	this.progress = goog.dom.createDom("div", {"id" : "Progress"});
+	goog.dom.appendChild(this.Element, this.progress);
 }
 
 //extend dispoable
@@ -23,5 +32,21 @@ goog.inherits(GamescreenTopBar, goog.Disposable);
 
 /** @override */
 GamescreenTopBar.prototype.disposeInternal = function(){
+	goog.dom.removeChildren(this.Element);
+	goog.dom.removeNode(this.Element);
+	this.Element = null;
+	this.progress = null;
 	goog.base(this, 'disposeInternal');
 }
+
+/** 
+	@param {number} stage
+	@param {number} level
+ */
+GamescreenTopBar.prototype.setStage = function(stage, level){
+	level += 1;
+	var levelCount = StageController.getLevelCount(stage);
+	var prog = goog.string.buildString(level, "/", levelCount);
+	goog.dom.setTextContent(this.progress, prog);
+}
+
