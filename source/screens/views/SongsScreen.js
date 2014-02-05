16c6e9a3c9f0e3a1e0d99bb19f9e512a1461fed6
Chris Deaner <deaner@monsterbit.com>
2014-02-05 01:22:58 -0500

    tracking some screen data

--- source/screens/views/SongsScreen.js
+++ source/screens/views/SongsScreen.js
@@ -49,6 +49,9 @@ var SongsScreen =  {
 	scrollStartPosition : NaN,
 	/** initializer */
 	initialize : function(){
+		// track that we are here
+		ga_storage._trackEvent('SongScreen', 'loaded', 'song_label', 'timestamp_here');
+
 		SongsScreen.Stages = StageController.Stages;
 		SongsScreen.songSelectedCb = StageController.songSelectedCb;
 
@@ -254,6 +257,9 @@ var SongsScreen =  {
 		Shows the screen
 	*/
 	showScreen : function(){
+		// track that we are showing the screen
+		ga_storage._trackEvent('SongScreen', 'show', 'song_label_show', 'timestamp_here');
+
 		goog.style.setElementShown(SongsScreen.div, true);
 		SongsScreen.updateSongButtons();
 	},
