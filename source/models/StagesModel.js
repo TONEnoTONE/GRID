b96b19de546018401ea0f2aec131aa970847b28c
Yotam Mann <yotammann@gmail.com>
2014-02-14 12:12:25 -0500

    new local storage name

--- source/models/StagesModel.js
+++ source/models/StagesModel.js
@@ -29,7 +29,7 @@ var StagesModel =  {
 	/** @type {goog.storage.mechanism.HTML5LocalStorage} */
 	storage : new goog.storage.mechanism.HTML5LocalStorage(),
 	/** @type {string}*/
-	storageStagesName : "StagesStatus4",	
+	storageStagesName : "StagesStatus5",	
 	/** initializer */
 	initialize : function(){
 		//setup the StagesStatus
