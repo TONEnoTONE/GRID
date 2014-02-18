5966ee5c54537c7801b713a84ca84a835be96ab2
Chris Deaner <deaner@monsterbit.com>
2014-02-18 16:11:04 -0500

    removed the gh pages build stuff and pointed to Echo Dev for the hydrating build. updated to the latest build of cordova

--- scripts/deploy/phonegap/phoneGapDeploy.sh
+++ scripts/deploy/phonegap/phoneGapDeploy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# phonegap appid 619681 is ECHO
+# phonegap appid 785172 is ECHO Dev
+pgAppId=785172
 phoneGapToken=${PHONEGAP_TOKEN}
 
 cp ./scripts/deploy/phonegap/index--PG_iphone--.html ./tmp/www/index.html
@@ -12,19 +16,11 @@ zip -r GRIDunLOCK.zip www
 echo "\n########################################"
 echo "uploading zip to phonegap"
 echo "########################################"
-# phonegap appid 619681 is ECHO
-# phonegap appid 708557 is Reflects
-curl -X PUT -F file=@./GRIDunLOCK.zip https://build.phonegap.com/api/v1/apps/619681?auth_token=$phoneGapToken
-#curl -X PUT -F file=@./GRIDunLOCK.zip https://build.phonegap.com/api/v1/apps/708557?auth_token=$phoneGapToken
-
-
+curl -X PUT -F file=@./GRIDunLOCK.zip https://build.phonegap.com/api/v1/apps/$pgAppId?auth_token=$phoneGapToken
 
 echo "\n########################################"
 echo "unlock the signing key"
 echo "########################################"
-# phonegap appid 619681 is ECHO
-# phonegap appid 708557 is Reflects
-
 #curl -X PUT -d 'data={"keys":{"ios":{"id":110067,"password":"grid"}}}' https://build.phonegap.com/api/v1/apps/619681?auth_token=$phoneGapToken
 #curl -X PUT -d 'data={"keys":{"ios":{"id":110067,"password":"grid"}}}' https://build.phonegap.com/api/v1/apps/708557?auth_token=$phoneGapToken
 
